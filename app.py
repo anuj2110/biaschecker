e744a08a2829bf5acb327d22e20bb80d8ec6440e
anuj2110 <atrehan789@gmail.com>
2020-10-27 15:47:37 +0530

    changes to app

--- app.py
+++ app.py
@@ -14,30 +14,33 @@ if option == "Instructions":
         1. Go to Try the bias checker option from the 
         2. Upload the csv you want to check bias for and wait for the results to be processed
     """)
-# elif option == "Example":
-#     st.write("""
-#         ## Below is an example showing how the results will be displayed.
-#     """)
-#     df = pd.read_csv("student-mat.csv")
-#     st.dataframe(df.style.highlight_max(axis=0))
-#     with open("df","wb") as f:
-#         pickle.dump(df,f)
-#         os.system('python model.py')
+elif option == "Example":
+    st.write("""
+        ## Below is an example showing how the results will be displayed.
+    """)
+    df = pd.read_csv("student-mat.csv")
+    st.dataframe(df.style.highlight_max(axis=0))
+    with open("df","wb") as f:
+        pickle.dump(df,f)
+    os.system('python model.py')
                     
-#     with open("percent", "rb") as f:
-#         percent= pickle.load(f)
+    with open("percent", "rb") as f:
+        percent= pickle.load(f)
                     
-#     with open("bias", "rb") as f:
-#         bias = pickle.load(f)
-#     if(len(bias)==0 or len(percent)==0):
-#         st.markdown('**The dataset does not have any _bias_**')
-#     else:
-#         st.markdown('**The _bias_ exists in**')
-#         for b in bias:
-#             st.subheader(b)
-#         st.write('**The class distribution in the categorical columns is**')
-#         for p in percent:
-#             st.dataframe(p,500,400)
+    with open("bias", "rb") as f:
+        bias = pickle.load(f)
+    if(len(bias)==0 or len(percent)==0):
+        st.markdown('**The dataset does not have any _bias_**')
+    else:
+        st.markdown('**The _bias_ exists in**')
+        for b in bias:
+            st.subheader(b)
+        st.write('**The class distribution in the categorical columns is**')
+        for p in percent:
+            st.dataframe(p,500,400)
+    os.remove("df")
+    os.remove("bias")
+    os.remove("percent")
 elif option == "Try the bias checker":
     df=None
     multiple_files = st.file_uploader('Enter a csv file',type=["csv"])
@@ -46,26 +49,29 @@ elif option == "Try the bias checker":
         df =df_
     if df is not None:
         st.dataframe(df.style.highlight_max(axis=0))
-        with open("df","wb") as f:
-            pickle.dump(df,f)
-        os.system('python model.py')
-                    
-        with open("percent", "rb") as f:
-            percent= pickle.load(f)
-                    
-        with open("bias", "rb") as f:
-            bias = pickle.load(f)
-        if(len(bias)==0 or len(percent)==0):
-            st.markdown('**The dataset does not have any _bias_**')
-        else:
-            st.markdown('**The _bias_ exists in**')
-            for b in bias:
-                st.subheader(b)
-            st.write('**The class distribution in the categorical columns is**')
-            for p in percent:
-                st.dataframe(p,500,400)
-    os.remove("df")
-    os.remove("bias")
-    os.remove("percent")
-        
+        try:
+            with open("df","wb") as f:
+                pickle.dump(df,f)
+            os.system('python model.py')
+                            
+            with open("percent", "rb") as f:
+                percent= pickle.load(f)
+                            
+            with open("bias", "rb") as f:
+                bias = pickle.load(f)
+            if(len(bias)==0 or len(percent)==0):
+                st.markdown('**The dataset does not have any _bias_**')
+            else:
+                st.markdown('**The _bias_ exists in**')
+                for b in bias:
+                    st.subheader(b)
+                st.write('**The class distribution in the categorical columns is**')
+                for p in percent:
+                    st.dataframe(p,500,400)
+            
+            os.remove("df")
+            os.remove("bias")
+            os.remove("percent")
+        except Exception as e:
+            pass
 
