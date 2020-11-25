f009020949a11eee985dcd1c82ff95d0e0721f1c
piyush-98 <arjunguptaa98@gmail.com>
2020-11-25 16:57:22 +0530

    added gretel

--- app.py
+++ app.py
@@ -218,12 +218,12 @@ elif option == "Try the bias checker":# This is the main page of the app
                         st.write('*class distributions*')
                         for p in percent:
                             st.dataframe(p,500,400)
-                        st.markdown('**Running the gretel synthetic data generator**')
+                        st.header('**Running the gretel synthetic data generator**')
                         st.markdown("**Synthetic dataset produced**")
                         syn_df_=pd.read_csv('synthetic-data-bank.csv')
                         syn_df =syn_df_
                         st.dataframe(syn_df.style.highlight_max(axis=0))
-                        st.header("**click on the below link to see the report for the synthetic**")
+                        st.markdown("**click on the below link to see the report for the synthetic data**")
                         report_tag="<a>https://anuj2110.github.io/biaschecker/</a>"
                         st.markdown(report_tag,unsafe_allow_html=True)
                         os.remove("df")
